4e5d1329abe602bb8ac8177fef8901d5c24747e3
kidthales <kidthales@agogpixel.com>
2026-04-01 22:24:09 -0700

    Separate template functions.

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,8 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { resolve } = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
-const vm = require('vm');
+
+const templateFunctions = require('./templates/functions');
 
 module.exports = async (env, args) => {
   const mode = args.mode || 'production';
@@ -82,7 +83,7 @@ module.exports = async (env, args) => {
             'html-loader',
             {
               loader: 'template-ejs-loader',
-              options: { async: true, data: { fetchPGMMVPluginData } }
+              options: { async: true, data: { ...templateFunctions } }
             }
           ]
         },
@@ -153,78 +154,3 @@ module.exports = async (env, args) => {
     }
   };
 };
-
-async function fetchPGMMVPluginData(pluginIdentifier) {
-  const latestReleaseResponse = await fetch(
-    `https://api.github.com/repos/kidthales/${pluginIdentifier}/releases/latest`
-  );
-
-  const releases = await (await fetch(`https://api.github.com/repos/kidthales/${pluginIdentifier}/releases`)).json();
-
-  const latestRelease = latestReleaseResponse.status !== 200 ? releases[0] : await latestReleaseResponse.json();
-
-  const iife = await (await fetch(latestRelease.assets[0].browser_download_url)).text();
-  const info = vm.runInThisContext(`
-      var plugin = ${iife}
-      var info = {
-        name: plugin.getInfo('name'),
-        description: plugin.getInfo('description'),
-        author: plugin.getInfo('author'),
-        help: plugin.getInfo('help'),
-        parameter: plugin.getInfo('parameter'),
-        internal: plugin.getInfo('internal'),
-        actionCommand: plugin.getInfo('actionCommand'),
-        linkCondition: plugin.getInfo('linkCondition'),
-      };
-      info // Last expression returned
-    `);
-
-  function normalizeParameters(parameters) {
-    return parameters.map(function (param) {
-      const p = {
-        name: param.name.replace(`[${info.name}]`, '').trim()
-      };
-
-      switch (param.type) {
-        case 'SwitchVariableObjectId':
-          p.value = 'Project Common';
-          param.option.forEach(function (option) {
-            switch (option) {
-              case 'SelfObject':
-                p.value += ', Object Self';
-                break;
-              case 'ParentObject':
-                p.value += ', Parent Object';
-                break;
-            }
-          });
-          break;
-        case 'CustomId':
-          p.value = param.customParam.reduce(function (v, cp) {
-            v += !v ? cp.name : `, ${cp.name}`;
-            return v;
-          }, '');
-          break;
-        default:
-          p.value = param.type;
-          break;
-      }
-
-      return p;
-    });
-  }
-
-  info.parameter = normalizeParameters(info.parameter);
-
-  info.actionCommand = info.actionCommand.map(function (ac) {
-    ac.parameter = normalizeParameters(ac.parameter);
-    return ac;
-  });
-
-  info.linkCondition = info.linkCondition.map(function (lc) {
-    lc.parameter = normalizeParameters(lc.parameter);
-    return lc;
-  });
-
-  return { info, latestRelease, releases, repoUrl: `https://github.com/kidthales/${pluginIdentifier}` };
-}
